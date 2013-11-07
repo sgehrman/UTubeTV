8ec599ac93dbb896760572ae2529683a6e318e80
Steve Gehrman <sgehrman@gmail.com>
2013-11-07 12:51:14 -0800

    h

---
+++ SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/AnimatedFrameLayout.java
@@ -0,0 +1,35 @@
+package com.sickboots.sickvideos;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * This was added so we could do a custom sliding animation for fragments.
+ */
+public class AnimatedFrameLayout extends FrameLayout {
+
+  public AnimatedFrameLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public float getXFraction() {
+    return getX() / getWidth();
+  }
+
+  public void setXFraction(float xFraction) {
+    final int width = getWidth();
+    setX((width > 0) ? (xFraction * width) : -9999);
+  }
+
+  public float getYFraction() {
+    return getY() / getHeight();
+  }
+
+  public void setYFraction(float yFraction) {
+    final int height = getHeight();
+    setY((height > 0) ? (yFraction * height) : -9999);
+  }
+
+}
+
