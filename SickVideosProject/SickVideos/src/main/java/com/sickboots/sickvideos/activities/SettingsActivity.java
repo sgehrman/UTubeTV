0aa099bb750c9aff9d09620d0d33717774da58c3
Steve Gehrman <sgehrman@gmail.com>
2013-12-20 02:20:52 -0800

    h

--- SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/activities/SettingsActivity.java
+++ SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/activities/SettingsActivity.java
@@ -1,11 +1,14 @@
 package com.sickboots.sickvideos.activities;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.view.MenuItem;
 
 import com.sickboots.sickvideos.R;
+import com.sickboots.sickvideos.misc.Debug;
 
 public class SettingsActivity extends Activity {
 
@@ -42,6 +45,20 @@ public class SettingsActivity extends Activity {
 
       // Load the preferences from an XML resource
       addPreferencesFromResource(R.xml.preferences);
+
+      try {
+        Preference pref = findPreference("version");
+
+        if (pref != null) {
+          PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+
+          pref.setTitle("Version: " + pInfo.versionName);
+          pref.setSummary("Code: " + pInfo.versionCode);
+        }
+
+      } catch (Throwable throwable) {
+        Debug.log("exception: " + throwable.getMessage());
+      }
     }
 
   }
