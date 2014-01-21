20f4f424a68b17e0d9ce1eb5ab9c517a75268ecf
Steve Gehrman <sgehrman@gmail.com>
2014-01-21 15:21:18 -0800

    h

--- SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/database/YouTubeData.java
+++ SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/database/YouTubeData.java
@@ -8,6 +8,8 @@ import android.text.style.StyleSpan;
 
 import com.google.api.client.util.DateTime;
 
+import org.ocpsoft.prettytime.PrettyTime;
+
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -15,8 +17,8 @@ import java.util.Date;
  * Created by sgehrman on 11/26/13.
  */
 public class YouTubeData {
-  private static final DateFormat sDateFormatter = DateFormat.getDateInstance();
-//  private static final PrettyTime sDateFormatter = new PrettyTime();
+//  private static final DateFormat sDateFormatter = DateFormat.getDateInstance();
+  private static final PrettyTime sDateFormatter = new PrettyTime();
 
   // raw access for speed
   public long mID;
