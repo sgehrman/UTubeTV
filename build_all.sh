af6bcc2016d5a9f14733bce2fb0fe5d88f3f8c00
Steve Gehrman <sgehrman@gmail.com>
2014-01-21 16:02:33 -0800

    h

---
+++ build_all.sh
@@ -0,0 +1,16 @@
+cd SickVideosProject
+
+# first clean all
+./gradlew clean
+
+# now build each one
+./gradlew assembleNeurosoupRelease
+./gradlew assembleMaxkeiserRelease
+./gradlew assembleViceRelease
+./gradlew assembleCodeorgRelease
+./gradlew assembleKhanRelease
+./gradlew assembleNerdistRelease
+./gradlew assembleAndroiddevsRelease
+./gradlew assembleRtRelease
+./gradlew assembleJoeroganRelease
+
