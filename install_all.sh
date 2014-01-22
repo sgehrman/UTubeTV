3d83bff95bfe48df876af24e491917ba8f2ed5e3
Steve Gehrman <sgehrman@gmail.com>
2014-01-21 16:25:28 -0800

    h

---
+++ install_all.sh
@@ -0,0 +1,12 @@
+cd SickVideosProject/SickVideos/build/apk
+
+# now install each one
+adb install SickVideos-codeorg-release.apk
+adb install SickVideos-neurosoup-release.apk
+adb install SickVideos-rt-release.apk
+adb install SickVideos-vice-release.apk
+adb install SickVideos-maxkeiser-release.apk
+adb install SickVideos-androiddevs-release.apk
+adb install SickVideos-khan-release.apk
+adb install SickVideos-nerdist-release.apk
+adb install SickVideos-joerogan-release.apk
---
+++ uninstall_all.sh
@@ -0,0 +1,11 @@
+
+adb uninstall com.sickboots.sickvideos.joerogan 
+adb uninstall com.sickboots.sickvideos.khan
+adb uninstall com.sickboots.sickvideos.nerdist
+adb uninstall com.sickboots.sickvideos.maxkeiser
+adb uninstall com.sickboots.sickvideos.androiddevs
+adb uninstall com.sickboots.sickvideos.codeorg
+adb uninstall com.sickboots.sickvideos.rt
+adb uninstall com.sickboots.sickvideos.vice
+adb uninstall com.sickboots.sickvideos.neurosoup
+
