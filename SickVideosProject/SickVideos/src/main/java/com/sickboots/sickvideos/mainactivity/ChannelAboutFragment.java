160c383feeca2b6afced187bde6c483451f6d0fc
Steve Gehrman <sgehrman@gmail.com>
2014-02-20 14:42:18 -0800

    nothing

--- SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/mainactivity/ChannelAboutFragment.java
+++ SickVideosProject/SickVideos/src/main/java/com/sickboots/sickvideos/mainactivity/ChannelAboutFragment.java
@@ -1,11 +1,16 @@
 package com.sickboots.sickvideos.mainactivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -17,6 +22,7 @@ import com.sickboots.sickvideos.database.YouTubeData;
 import com.sickboots.sickvideos.imageutils.BitmapLoader;
 import com.sickboots.sickvideos.misc.EmptyListHelper;
 import com.sickboots.sickvideos.misc.Events;
+import com.sickboots.sickvideos.misc.Utils;
 
 import de.greenrobot.event.EventBus;
 import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
@@ -60,6 +66,7 @@ public class ChannelAboutFragment extends Fragment implements OnRefreshListener 
     mContentView = rootView.findViewById(R.id.content_view);
 
     Button button = (Button) rootView.findViewById(R.id.watch_button);
+    Button disclaimer = (Button) rootView.findViewById(R.id.disclaimer);
     LinearLayout card = (LinearLayout) rootView.findViewById(R.id.card);
 
     card.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +77,11 @@ public class ChannelAboutFragment extends Fragment implements OnRefreshListener 
     button.setOnClickListener(new View.OnClickListener() {
       public void onClick(View v) {
         showPlaylistsFragment();
+      }
+    });
+    disclaimer.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        showDisclaimer();
       }
     });
 
@@ -121,6 +133,35 @@ public class ChannelAboutFragment extends Fragment implements OnRefreshListener 
     updateUI();
     mPullToRefreshLayout.setRefreshComplete();
   }
+
+  private void showDisclaimer() {
+    String title = "Disclaimer";
+    String channelName = "channel";
+
+    final YouTubeData data = mContent.channelInfo();
+    if (data != null)
+      channelName = data.mTitle;
+
+    String message = Utils.getApplicationName(getActivity());
+    message += " is not affiliated with " + channelName + ".";
+    message += "\n\nInformation shown in this app is obtained through public YouTube APIs.  We are fans of " + channelName + " and we wanted a better viewing experience to watch their videos.";
+    message += "\n\nAny copyrighted material belongs to the original owners. If you want changes made to this app, please let us know.";
+
+      final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setTitle(title)
+          .setMessage(message)
+          .setPositiveButton("Close", new Dialog.OnClickListener() {
+            public void onClick(final DialogInterface dialogInterface, final int i) {
+              dialogInterface.dismiss();
+            }
+          })
+          .setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+              dialog.dismiss();
+            }
+          });
+      builder.create().show();
+    }
 
   private void updateUI() {
     final YouTubeData data = mContent.channelInfo();
