030ff36b0a0aa32de29225cf58ce6c28b08cd681
Steve Gehrman <sgehrman@gmail.com>
2014-03-10 18:50:17 -0700

    nothing

--- SickVideosProject/tools/build_all.sh
+++ SickVideosProject/tools/build_all.sh
@@ -6,24 +6,24 @@ cd ..
 ./gradlew clean
 
 # now build each one
-./gradlew assembleTechnewsRelease
-./gradlew assembleNerdistRelease
+# ./gradlew assembleTechnewsRelease
+# ./gradlew assembleNerdistRelease
+./gradlew assembleNeurosoupRelease
+./gradlew assembleMaxkeiserRelease
+./gradlew assembleAndroiddevsRelease
+./gradlew assembleJoeroganRelease
+./gradlew assembleJetdaisukeRelease
+./gradlew assembleBigthinkRelease
+./gradlew assembleThevergeRelease
+./gradlew assembleReasontvRelease
 
-#./gradlew assembleNeurosoupRelease
-#./gradlew assembleMaxkeiserRelease
 #./gradlew assembleViceRelease
 #./gradlew assembleCodeorgRelease
 #./gradlew assembleKhanRelease
-#./gradlew assembleAndroiddevsRelease
 #./gradlew assembleRtRelease
-#./gradlew assembleJoeroganRelease
-#./gradlew assembleJetdaisukeRelease
 #./gradlew assembleTopgearRelease
-#./gradlew assembleBigthinkRelease
 #./gradlew assemblePewdiepieRelease
-#./gradlew assembleReasontvRelease
 #./gradlew assembleXdaRelease
-#./gradlew assembleThevergeRelease
 #./gradlew assembleJustinbieberRelease
 
 
--- SickVideosProject/tools/uninstall_all.sh
+++ SickVideosProject/tools/uninstall_all.sh
@@ -4,25 +4,24 @@ for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
   do
     echo $DEVICE
 
-    adb -s $DEVICE uninstall com.distantfuture.videos.technews
-    adb -s $DEVICE uninstall com.sickboots.sickvideos.nerdist
+#    adb -s $DEVICE uninstall com.distantfuture.videos.technews
+#    adb -s $DEVICE uninstall com.sickboots.sickvideos.nerdist
+    adb -s $DEVICE uninstall com.distantfuture.videos.neurosoup
+    adb -s $DEVICE uninstall com.distantfuture.videos.maxkeiser
+    adb -s $DEVICE uninstall com.distantfuture.videos.androiddevs
+    adb -s $DEVICE uninstall com.distantfuture.videos.joerogan
+    adb -s $DEVICE uninstall com.distantfuture.videos.jetdaisuke
+    adb -s $DEVICE uninstall com.distantfuture.videos.bigthink
+    adb -s $DEVICE uninstall com.distantfuture.videos.theverge
+    adb -s $DEVICE uninstall com.distantfuture.videos.reasontv
 
-#    adb -s $DEVICE uninstall com.distantfuture.videos.joerogan
 #    adb -s $DEVICE uninstall com.distantfuture.videos.khan
-#    adb -s $DEVICE uninstall com.distantfuture.videos.nerdist
-#    adb -s $DEVICE uninstall com.distantfuture.videos.maxkeiser
-#    adb -s $DEVICE uninstall com.distantfuture.videos.androiddevs
 #    adb -s $DEVICE uninstall com.distantfuture.videos.codeorg
 #    adb -s $DEVICE uninstall com.distantfuture.videos.rt
 #    adb -s $DEVICE uninstall com.distantfuture.videos.vice
-#    adb -s $DEVICE uninstall com.distantfuture.videos.neurosoup
-#    adb -s $DEVICE uninstall com.distantfuture.videos.jetdaisuke
 #    adb -s $DEVICE uninstall com.distantfuture.videos.topgear
-#    adb -s $DEVICE uninstall com.distantfuture.videos.bigthink
 #    adb -s $DEVICE uninstall com.distantfuture.videos.pewdiepie
-#    adb -s $DEVICE uninstall com.distantfuture.videos.reasontv
 #    adb -s $DEVICE uninstall com.distantfuture.videos.xda
-#    adb -s $DEVICE uninstall com.distantfuture.videos.theverge
 #    adb -s $DEVICE uninstall com.distantfuture.videos.justinbieber
 
   done
