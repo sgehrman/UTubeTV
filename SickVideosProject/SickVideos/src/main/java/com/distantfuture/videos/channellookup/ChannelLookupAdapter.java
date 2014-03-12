75c5fb9224f3a1bf05b19a2517ae5562579337ea
Steve Gehrman <sgehrman@gmail.com>
2014-03-12 16:21:30 -0700

    nothing

--- SickVideosProject/SickVideos/src/main/java/com/distantfuture/videos/channellookup/ChannelLookupAdapter.java
+++ SickVideosProject/SickVideos/src/main/java/com/distantfuture/videos/channellookup/ChannelLookupAdapter.java
@@ -1,6 +1,8 @@
 package com.distantfuture.videos.channellookup;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 import com.androidquery.AQuery;
 import com.distantfuture.videos.R;
 import com.distantfuture.videos.database.YouTubeData;
+import com.distantfuture.videos.imageutils.ToolbarIcons;
+import com.distantfuture.videos.misc.Debug;
 
 import java.util.List;
 
@@ -18,10 +22,21 @@ public class ChannelLookupAdapter extends ArrayAdapter<YouTubeData> {
 
   private final Context mContext;
   private final float mAspectRatio = 9f / 16f;
+  private BitmapDrawable mPlusButtonBitmap;
+  View.OnClickListener mButtonListener;
 
   public ChannelLookupAdapter(Context context) {
     super(context, 0);
     this.mContext = context;
+
+    mPlusButtonBitmap = ToolbarIcons.iconBitmap(context, ToolbarIcons.IconID.CLOSE, Color.RED, 36);
+
+    mButtonListener = new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Debug.log("fuck you");
+      }
+    };
   }
 
   @Override
@@ -32,12 +47,15 @@ public class ChannelLookupAdapter extends ArrayAdapter<YouTubeData> {
     YouTubeData mm = getItem(position);
 
     if (convertView == null) {
-      convertView = inflater.inflate(R.layout.browse_row, null);
+      convertView = inflater.inflate(R.layout.channel_lookup_list_item, null);
       holder = new ViewHolder();
       holder.imgView = (ImageView) convertView.findViewById(R.id.imageView1);
+      holder.addButton = (ImageView) convertView.findViewById(R.id.add_remove_button);
       holder.titleView = (TextView) convertView.findViewById(R.id.textView1);
       holder.descrView = (TextView) convertView.findViewById(R.id.textView2);
       convertView.setTag(holder);
+
+      holder.addButton.setOnClickListener(mButtonListener);
     } else {
       holder = (ViewHolder) convertView.getTag();
     }
@@ -45,9 +63,11 @@ public class ChannelLookupAdapter extends ArrayAdapter<YouTubeData> {
     AQuery aq = new AQuery(convertView);
     aq.id(holder.imgView)
         .width(110)
-        .image(mm.mThumbnail, true, true, 0, R.drawable.default_video, null, 0, mAspectRatio);
+        .image(mm.mThumbnail, true, true, 0, 0, null, 0, mAspectRatio);
     aq.id(holder.titleView).text(mm.mTitle);
     aq.id(holder.descrView).text(mm.mDescription);
+
+    holder.addButton.setImageDrawable(mPlusButtonBitmap);
 
     return convertView;
   }
@@ -56,6 +76,7 @@ public class ChannelLookupAdapter extends ArrayAdapter<YouTubeData> {
     TextView titleView;
     TextView descrView;
     ImageView imgView;
+    ImageView addButton;
   }
 
   public void setData(List<YouTubeData> data) {
