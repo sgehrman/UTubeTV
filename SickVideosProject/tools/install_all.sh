5d62b51e4c77d628556ccf94d22d56d3f0248bca
Steve Gehrman <sgehrman@gmail.com>
2014-07-16 18:23:55 -0700

    fixed build scripts

--- SickVideosProject/tools/install_all.sh
+++ SickVideosProject/tools/install_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ../SickVideos/build/apk
+cd ../SickVideos/build/outputs/apk
 
 for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
   do
@@ -8,25 +8,25 @@ for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
 
     adb -s $DEVICE install SickVideos-technews-release.apk
 
-#    adb -s $DEVICE install SickVideos-youtube-release.apk
-#    adb -s $DEVICE install SickVideos-svb-release.apk
-#    adb -s $DEVICE install SickVideos-nerdist-release.apk
-#    adb -s $DEVICE install SickVideos-neurosoup-release.apk
-#    adb -s $DEVICE install SickVideos-maxkeiser-release.apk
-#    adb -s $DEVICE install SickVideos-androiddevs-release.apk
-#    adb -s $DEVICE install SickVideos-joerogan-release.apk
-#    adb -s $DEVICE install SickVideos-jetdaisuke-release.apk
-#    adb -s $DEVICE install SickVideos-bigthink-release.apk
-#    adb -s $DEVICE install SickVideos-theverge-release.apk
-#    adb -s $DEVICE install SickVideos-reasontv-release.apk
-#    adb -s $DEVICE install SickVideos-codeorg-release.apk
-#    adb -s $DEVICE install SickVideos-rt-release.apk
-#    adb -s $DEVICE install SickVideos-vice-release.apk
-#    adb -s $DEVICE install SickVideos-khan-release.apk
-#    adb -s $DEVICE install SickVideos-topgear-release.apk
-#    adb -s $DEVICE install SickVideos-pewdiepie-release.apk
-#    adb -s $DEVICE install SickVideos-xda-release.apk
-#    adb -s $DEVICE install SickVideos-justinbieber-release.apk
+    adb -s $DEVICE install SickVideos-youtube-release.apk
+    adb -s $DEVICE install SickVideos-svb-release.apk
+    adb -s $DEVICE install SickVideos-nerdist-release.apk
+    adb -s $DEVICE install SickVideos-neurosoup-release.apk
+    adb -s $DEVICE install SickVideos-maxkeiser-release.apk
+    adb -s $DEVICE install SickVideos-androiddevs-release.apk
+    adb -s $DEVICE install SickVideos-joerogan-release.apk
+    adb -s $DEVICE install SickVideos-jetdaisuke-release.apk
+    adb -s $DEVICE install SickVideos-bigthink-release.apk
+    adb -s $DEVICE install SickVideos-theverge-release.apk
+    adb -s $DEVICE install SickVideos-reasontv-release.apk
+    adb -s $DEVICE install SickVideos-codeorg-release.apk
+    adb -s $DEVICE install SickVideos-rt-release.apk
+    adb -s $DEVICE install SickVideos-vice-release.apk
+    adb -s $DEVICE install SickVideos-khan-release.apk
+    adb -s $DEVICE install SickVideos-topgear-release.apk
+    adb -s $DEVICE install SickVideos-pewdiepie-release.apk
+    adb -s $DEVICE install SickVideos-xda-release.apk
+    adb -s $DEVICE install SickVideos-justinbieber-release.apk
 
   done
 
--- SickVideosProject/tools/uninstall_all.sh
+++ SickVideosProject/tools/uninstall_all.sh
@@ -5,26 +5,26 @@ for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
     echo $DEVICE
 
     adb -s $DEVICE uninstall re.distantfutu.videos.technews
-#    adb -s $DEVICE uninstall re.distantfutu.videos.youtube
-#    adb -s $DEVICE uninstall re.distantfutu.videos.svb
+    adb -s $DEVICE uninstall re.distantfutu.videos.youtube
+    adb -s $DEVICE uninstall re.distantfutu.videos.svb
 
-#    adb -s $DEVICE uninstall re.distantfutu.videos.nerdist
-#    adb -s $DEVICE uninstall re.distantfutu.videos.neurosoup
-#    adb -s $DEVICE uninstall re.distantfutu.videos.maxkeiser
-#    adb -s $DEVICE uninstall re.distantfutu.videos.androiddevs
-#    adb -s $DEVICE uninstall re.distantfutu.videos.joerogan
-#    adb -s $DEVICE uninstall re.distantfutu.videos.jetdaisuke
-#    adb -s $DEVICE uninstall re.distantfutu.videos.bigthink
-#    adb -s $DEVICE uninstall re.distantfutu.videos.theverge
-#    adb -s $DEVICE uninstall re.distantfutu.videos.reasontv
-#    adb -s $DEVICE uninstall re.distantfutu.videos.khan
-#    adb -s $DEVICE uninstall re.distantfutu.videos.codeorg
-#    adb -s $DEVICE uninstall re.distantfutu.videos.rt
-#    adb -s $DEVICE uninstall re.distantfutu.videos.vice
-#    adb -s $DEVICE uninstall re.distantfutu.videos.topgear
-#    adb -s $DEVICE uninstall re.distantfutu.videos.pewdiepie
-#    adb -s $DEVICE uninstall re.distantfutu.videos.xda
-#    adb -s $DEVICE uninstall re.distantfutu.videos.justinbieber
+    adb -s $DEVICE uninstall re.distantfutu.videos.nerdist
+    adb -s $DEVICE uninstall re.distantfutu.videos.neurosoup
+    adb -s $DEVICE uninstall re.distantfutu.videos.maxkeiser
+    adb -s $DEVICE uninstall re.distantfutu.videos.androiddevs
+    adb -s $DEVICE uninstall re.distantfutu.videos.joerogan
+    adb -s $DEVICE uninstall re.distantfutu.videos.jetdaisuke
+    adb -s $DEVICE uninstall re.distantfutu.videos.bigthink
+    adb -s $DEVICE uninstall re.distantfutu.videos.theverge
+    adb -s $DEVICE uninstall re.distantfutu.videos.reasontv
+    adb -s $DEVICE uninstall re.distantfutu.videos.khan
+    adb -s $DEVICE uninstall re.distantfutu.videos.codeorg
+    adb -s $DEVICE uninstall re.distantfutu.videos.rt
+    adb -s $DEVICE uninstall re.distantfutu.videos.vice
+    adb -s $DEVICE uninstall re.distantfutu.videos.topgear
+    adb -s $DEVICE uninstall re.distantfutu.videos.pewdiepie
+    adb -s $DEVICE uninstall re.distantfutu.videos.xda
+    adb -s $DEVICE uninstall re.distantfutu.videos.justinbieber
 
   done
 
